4d1c39824adffc6a294b20edd1c69d9d66f2bcb8
Federico Rampazzo <federamp91@gmail.com>
2021-03-14 12:02:19 -0300

    Terminado. Bug?
    Cuando el numero ingresado es lo suficientemente grande, el factorial pasa a ser 0, es por el tamaño del factorial? incluyo .txt con loop del codigo itinerando de 1-100

--- Repaso20/Repaso20/PrimosAnteriores.cs
+++ Repaso20/Repaso20/PrimosAnteriores.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Repaso20
 {
     public class PrimosAnteriores
     {
         public PrimosAnteriores()
         {
+        }
+
+        public static bool esPrimo(int num)
+        //boolean para chequear si un int es primo
+        {
+            if (num == 0) return false;
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static List<int> get(int limiteSuperior)
+        {
+            List<int> resultado = new List<int>();
+            for(int i = 1; i < limiteSuperior; i++)
+            {
+                if (esPrimo(i))
+                {
+                    resultado.Add(i);
+                }
+            }
+            return resultado;
         }
     }
 }
--- Repaso20/Repaso20/Program.cs
+++ Repaso20/Repaso20/Program.cs
@@ -6,7 +6,36 @@ namespace Repaso20
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(Factorial.get(0));
+            /*
+            string ingreso = "";
+            int numeroIngresado = -1;
+            do
+            {
+                Console.WriteLine("Ingresar un numero mayor a 1");
+                ingreso = Console.ReadLine();
+                if(!int.TryParse(ingreso, out numeroIngresado))
+                {
+                    Console.WriteLine("Un número, por favor");
+                    ingreso = "";
+                } else if(numeroIngresado < 1)
+                {
+                    Console.WriteLine("Mayor a 1, por favor");
+                    ingreso = "";
+                }
+            } while (ingreso == "");
+            */
+
+            for (int numeroIngresado = 1; numeroIngresado < 100; numeroIngresado++)
+            {
+                int cantidadPrimosAnteriores = PrimosAnteriores.get(numeroIngresado).Count - 1;
+                int factorialDelIngreso = Factorial.get(numeroIngresado);
+
+                string mensaje = "Hasta el " + numeroIngresado + " hay " +
+                                        cantidadPrimosAnteriores + " primos, y el factorial es "
+                                        + factorialDelIngreso;
+
+                Console.WriteLine(mensaje);
+            }
 
         }
     }
